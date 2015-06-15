27935199d94612ef24469c354144c51b117bf031
Xuyuan Pang <xuyuanp@gmail.com>
2015-06-15 16:35:57 +0800

    Added comments

--- safemap.go
+++ safemap.go
@@ -19,28 +19,33 @@ package safemap
 
 import "sync"
 
+// Map is goroutine-safe map.
 type Map struct {
 	mu sync.RWMutex
 	m  map[interface{}]interface{}
 }
 
+// New creates a new Map with the capacity.
 func New(capacity int) *Map {
 	return &Map{
 		m: make(map[interface{}]interface{}, capacity),
 	}
 }
 
+// Set sets a k-v paire.
 func (m *Map) Set(key, value interface{}) {
 	m.withLockContext(func() {
 		m.m[key] = value
 	})
 }
 
+// Get returns value binded with the key, nil if no value binded.
 func (m *Map) Get(key interface{}) (value interface{}) {
 	value, _ = m.GetOk(key)
 	return
 }
 
+// GetOk returns value binded with the key and true, or nil, fale if no value binded.
 func (m *Map) GetOk(key interface{}) (value interface{}, ok bool) {
 	m.withRLockContext(func() {
 		value, ok = m.m[key]
@@ -48,6 +53,8 @@ func (m *Map) GetOk(key interface{}) (value interface{}, ok bool) {
 	return
 }
 
+// GetMust returns value binded with the key,
+// if no value binded, creates a new value by useing the newFunc and adds the value in map.
 func (m *Map) GetMust(key interface{}, newFunc func() interface{}) (value interface{}) {
 	m.withLockContext(func() {
 		var ok bool
@@ -60,12 +67,14 @@ func (m *Map) GetMust(key interface{}, newFunc func() interface{}) (value interf
 	return
 }
 
+// Delete deletes the key in map.
 func (m *Map) Delete(key interface{}) {
 	m.withLockContext(func() {
 		delete(m.m, key)
 	})
 }
 
+// Len returns the map length.
 func (m *Map) Len() (length int) {
 	m.withRLockContext(func() {
 		length = len(m.m)
@@ -73,12 +82,14 @@ func (m *Map) Len() (length int) {
 	return length
 }
 
+// Reset clears the map.
 func (m *Map) Reset() {
 	m.withLockContext(func() {
 		m.m = make(map[interface{}]interface{})
 	})
 }
 
+// All returns all keys and values.
 func (m *Map) All() (keys, values []interface{}) {
 	m.withRLockContext(func() {
 		keys = make([]interface{}, len(m.m))
